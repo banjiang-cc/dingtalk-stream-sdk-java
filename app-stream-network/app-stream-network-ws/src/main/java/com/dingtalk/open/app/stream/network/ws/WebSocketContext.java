97f50c56c9749eca6a932ea490fae57aa2f75eef
Tairy <tairyguo@gmail.com>
2024-01-05 18:33:51 +0800

    [feat] change fastjson version.

--- app-stream-network/app-stream-network-ws/src/main/java/com/dingtalk/open/app/stream/network/ws/WebSocketContext.java
+++ app-stream-network/app-stream-network-ws/src/main/java/com/dingtalk/open/app/stream/network/ws/WebSocketContext.java
@@ -15,47 +15,54 @@ import io.netty.channel.ChannelHandlerContext;
  * @date 2023/3/27
  */
 public class WebSocketContext implements Context {
-    private static InternalLogger LOGGER = InternalLoggerFactory.getLogger(WebSocketContext.class);
-    private final ChannelHandlerContext context;
-    private final ProtocolRequestFacade request;
-    private final String connectionId;
+  private static InternalLogger LOGGER = InternalLoggerFactory.getLogger(WebSocketContext.class);
+  private final ChannelHandlerContext context;
+  private final ProtocolRequestFacade request;
+  private final String connectionId;
 
-    public WebSocketContext(String connectionId, ProtocolRequestFacade request, ChannelHandlerContext context) {
-        this.context = context;
-        this.connectionId = connectionId;
-        this.request = request;
-    }
+  public WebSocketContext(String connectionId, ProtocolRequestFacade request,
+                          ChannelHandlerContext context) {
+    this.context = context;
+    this.connectionId = connectionId;
+    this.request = request;
+  }
 
-    @Override
-    public void replay(Object payload) {
-        ProtocolResponse response = ProtocolResponse.new200Response(request);
-        response.setData(JSON.toJSONString(payload, SerializerFeature.WriteMapNullValue));
-        context.writeAndFlush(response).addListener(future -> {
-            if (!future.isSuccess()) {
-                LOGGER.error("[DingTalk] websocket connection reply response failed, connectionId={}, messageId={}", connectionId, request.getMessageId(), future.cause());
-                context.close();
-            }
-        });
-    }
+  @Override
+  public void replay(Object payload) {
+    ProtocolResponse response = ProtocolResponse.new200Response(request);
+    response.setData(JSON.toJSONString(payload, SerializerFeature.WriteMapNullValue));
+    context.writeAndFlush(response).addListener(future -> {
+      if (!future.isSuccess()) {
+        LOGGER.error(
+            "[DingTalk] websocket connection reply response failed, connectionId={}, messageId={}",
+            connectionId, request.getMessageId(), future.cause());
+        context.close();
+      }
+    });
+  }
 
-    @Override
-    public void exception(Throwable t) {
-        ProtocolResponse response = t instanceof ServiceException ? ProtocolResponse.newErrorResponse(request, ((ServiceException) t).getCode(), t.getMessage()) : ProtocolResponse.new500Response(request);
-        context.writeAndFlush(response).addListener(future -> {
-            if (!future.isSuccess()) {
-                LOGGER.error("[DingTalk] websocket connection reply response failed, connectionId={}, messageId={}", connectionId, request.getMessageId(), future.cause());
-                context.close();
-            }
-        });
-    }
+  @Override
+  public void exception(Throwable t) {
+    ProtocolResponse response = t instanceof ServiceException ?
+        ProtocolResponse.newErrorResponse(request, ((ServiceException) t).getCode(),
+            t.getMessage()) : ProtocolResponse.new500Response(request);
+    context.writeAndFlush(response).addListener(future -> {
+      if (!future.isSuccess()) {
+        LOGGER.error(
+            "[DingTalk] websocket connection reply response failed, connectionId={}, messageId={}",
+            connectionId, request.getMessageId(), future.cause());
+        context.close();
+      }
+    });
+  }
 
-    @Override
-    public String connectionId() {
-        return this.connectionId;
-    }
+  @Override
+  public String connectionId() {
+    return this.connectionId;
+  }
 
-    @Override
-    public ProtocolRequestFacade getRequest() {
-        return this.request;
-    }
+  @Override
+  public ProtocolRequestFacade getRequest() {
+    return this.request;
+  }
 }
